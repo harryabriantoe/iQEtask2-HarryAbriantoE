4eadd8ea9c07a8c62bf18d002bc38b4c4feac981
Harry Abrianto <harryabriantoe@gmail.com>
2021-11-06 19:38:05 +0700

    second commit

---
+++ ProgramSederhana/src/Kalkulator.java
@@ -0,0 +1,22 @@
+public class Kalkulator {
+    public static void main(String[] args) {
+        float bilangan1 = 3;
+        float bilangan2 = 1;
+
+        float penjumlahan = bilangan1 + bilangan1;
+        System.out.println("Penjumlahan");
+        System.out.println(penjumlahan);
+
+        float pengurangan = bilangan1 - bilangan2;
+        System.out.println("Pengurangan");
+        System.out.println(pengurangan);
+
+        float perkalian = bilangan1 * bilangan2;
+        System.out.println("perkalian");
+        System.out.println(perkalian);
+
+        float pembagian = bilangan1 / bilangan2;
+        System.out.println("pembagian");
+        System.out.println(pembagian);
+    }
+}
